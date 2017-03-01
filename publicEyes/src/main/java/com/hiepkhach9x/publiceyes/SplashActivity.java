e4705ade0ea5cb615eedc64add29ccbf677f58db
HungHN <hung.hoang@ntq-solution.com.vn>
2017-03-01 16:01:40 +0700

    create main activity

--- publicEyes/src/main/java/com/hiepkhach9x/publiceyes/SplashActivity.java
+++ publicEyes/src/main/java/com/hiepkhach9x/publiceyes/SplashActivity.java
@@ -1,13 +1,41 @@
 package com.hiepkhach9x.publiceyes;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Handler mHandler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        mHandler = new Handler();
+        mHandler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startMainActivity();
+            }
+        }, 1000);
+    }
+
+    private void startMainActivity() {
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+
+    private void startRegisterActivity() {
+        startActivity(new Intent(this, RegisterActivity.class));
+        finish();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mHandler.removeCallbacksAndMessages(null);
     }
 }
