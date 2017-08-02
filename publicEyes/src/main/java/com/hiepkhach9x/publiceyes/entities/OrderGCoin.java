993c93adddb45db7b9865aea93417fc2fc8de9a1
HungHN <hung.hoang@ntq-solution.com.vn>
2017-08-02 17:50:11 +0700

    GCoint response

---
+++ publicEyes/src/main/java/com/hiepkhach9x/publiceyes/entities/OrderGCoin.java
@@ -0,0 +1,100 @@
+package com.hiepkhach9x.publiceyes.entities;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by HungHN on 8/2/17.
+ */
+
+public class OrderGCoin {
+    @SerializedName("transRef")
+    private String transRef;
+    @SerializedName("sendOrderId")
+    private String sendOrderId;
+    @SerializedName("userNophone")
+    private String userNoPhone;
+    @SerializedName("amount")
+    private String amount;
+    @SerializedName("address")
+    private String address;
+    @SerializedName("createOn")
+    private String createdOn;
+    @SerializedName("time")
+    private String time;
+    @SerializedName("status")
+    private int status;
+    @SerializedName("callback_data")
+    private String callbackData;
+
+    public String getTransRef() {
+        return transRef;
+    }
+
+    public void setTransRef(String transRef) {
+        this.transRef = transRef;
+    }
+
+    public String getSendOrderId() {
+        return sendOrderId;
+    }
+
+    public void setSendOrderId(String sendOrderId) {
+        this.sendOrderId = sendOrderId;
+    }
+
+    public String getUserNoPhone() {
+        return userNoPhone;
+    }
+
+    public void setUserNoPhone(String userNoPhone) {
+        this.userNoPhone = userNoPhone;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(String createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getCallbackData() {
+        return callbackData;
+    }
+
+    public void setCallbackData(String callbackData) {
+        this.callbackData = callbackData;
+    }
+}
--- publicEyes/src/main/java/com/hiepkhach9x/publiceyes/task/ConvertPointHandler.java
+++
@@ -1,8 +0,0 @@
-package com.hiepkhach9x.publiceyes.task;
-
-/**
- * Created by HungHN on 8/2/17.
- */
-
-public class ConvertPointHandler {
-}
